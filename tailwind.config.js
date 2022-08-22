171900ebf4c4f865cee7c753fc1451ba5d80c647
Danny Venn <freedannyv@gmail.com>
2022-08-22 12:02:35 +0700

    updates image-with-text component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ module.exports = {
   prefix: "x",
   content: ["./**/*.{liquid,css}"],
   theme: {
-    screens: {
-      md: "750px",
-      lg: "990px",
-    },
     extend: {
       fontFamily: {
         heading: "var(--font-heading-family)",
